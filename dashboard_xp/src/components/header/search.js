c1e92e97ccc2a0a2e4428373772427a26f0cd015
Julien Savoie <julien@savoie.dev>
2020-03-09 14:37:14 -0400

    Added osrs-json-hiscores

--- dashboard_xp/src/components/header/search.js
+++ dashboard_xp/src/components/header/search.js
@@ -2,11 +2,24 @@ import React from "react";
 
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        console.log('Le lien a été cliqué.');
+    }
+
+
     render() {
         return (
             <div className="search">
                 <input className="search-input" placeholder="Type your username here..."></input>
-                <button className="search-enter">Search</button>
+                <button className="search-enter" onClick={this.handleClick}>Search</button>
             </div>
         );
     }
